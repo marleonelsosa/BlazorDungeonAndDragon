3f7241232edeef870bb38e444f029d5fd807a907
marleonelsosa@gmail.com <marleonelsosa@gmail.com>
2021-11-13 02:44:33 -0300

    Se crearon las Pages: Clases y Hechizos. Se creo la clase: Clase y los componentes ListadoClases y MostrarClase

---
+++ BlazorDungeonAndDragon/Shared/Entidades/Clase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDungeonAndDragon.Shared.Entidades
+{
+    public class Clase
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public int PuntosDeGolpe { get; set; }
+    }
+}
